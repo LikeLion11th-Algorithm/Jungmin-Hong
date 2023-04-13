733c2e7bfe3764e090a2f6bcd0114ac06868309d
Jungmin-Hong <jungmin011107@gmail.com>
2023-04-13 23:05:50 +0900

    solved: [PGS] 120812

--- 프로그래머스/120812: 최빈값 구하기.java
+++ 프로그래머스/120812: 최빈값 구하기.java
@@ -1,42 +1,31 @@
 class Solution {
     public int solution(int[] array) {
-        // 1. 배열에 숫자별 개수를 센다
-        // 2. 반복문을 돌려서 배열 중에 가장 큰 숫자를 가진 값을 찾아낸다.
-        // 3. 만약 배열 중 같은 값을 가진 쪽이 더 있다면 -1 반환.
-        int[] num = new int[1000];
         int answer = -1;
         int maxnum = 0;
-        int count = 0;
+        int[] count = new int[1000];
+        int num = 0;
         
-        for (int i=0; i<array.length; i++) {
-            num[array[i]]++;
-            answer = i;
-        }
-        if (array.length == 1){
-            return array[0];
-        }
-        else {
-            for (int i=0;i<array.length-1;i++) {
-                if (num[array[i]] > num[array[i+1]]){
-                    maxnum = num[array[i]];
-                 answer = array[i];
-                }
-                else {
-                    maxnum = num[array[i+1]];
-                    answer = array[i+1];
-                }
+        // 1. 배열에 숫자별 개수를 센다
+        for(int i = 0; i < array.length; i++) {
+            count[array[i]]++;
+            
+            // 2. 그 중에 원래보다 개수가 많아진 것이 생기면 교체
+            if(maxnum < count[array[i]]) {
+                maxnum = count[array[i]];
+                answer = array[i];
             }
+        }
         
-            for (int i=0; i<1000; i++) {
-                if (num[i] == maxnum) {
-                    count++;
-                }
+        // 3. 만약 배열 중 같은 값을 가진 쪽이 더 있다면 -1 반환.
+        for(int i = 0; i < 1000; i++) {
+            if(maxnum == count[i]) num++;
+            if(num > 1) {
+                answer = -1; 
+                break;
             }
-            if (count != 1) {
-                answer = -1;
-            }
-            return answer;
         }
+        
+        return answer;
         
     }
 }
