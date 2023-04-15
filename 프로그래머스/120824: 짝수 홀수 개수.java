94f07fb688ed3a2920925da7e21f4975837ed9b5
Jungmin-Hong <jungmin011107@gmail.com>
2023-04-15 15:17:19 +0900

    solved: [PGS] 120824

---
+++ 프로그래머스/120824: 짝수 홀수 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] solution(int[] num_list) {
+        int even_cnt = 0;
+        int odd_cnt = 0;
+        
+        for (int i=0; i<num_list.length; i++) {
+            if (num_list[i] % 2 == 0) {
+                even_cnt++;
+            }
+            else odd_cnt++;
+        }
+        
+        int[] answer = { even_cnt, odd_cnt };
+        return answer;
+    }
+}
