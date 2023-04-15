0d15ced3998934763da811a19c553630ee202609
Jungmin-Hong <jungmin011107@gmail.com>
2023-04-15 11:11:52 +0900

    solved: [PGS] 120818

---
+++ 프로그래머스/120818: 옷가게 할인 받기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int price) {
+        
+        if (price >= 100000 && price <300000) {
+            return price/100*95;
+        } 
+        if (price >= 300000 && price < 500000) {
+            return price/100*90;
+        } 
+        if (price >= 500000) {
+            return price/100*80;
+        }
+            
+        return price;
+    }
+}
