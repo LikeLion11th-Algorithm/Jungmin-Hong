75d4b3be23ad60cd522f5ca09f6d81741cfb5032
Jungmin-Hong <jungmin011107@gmail.com>
2023-04-25 13:15:48 +0900

    another: [PGS] 120889

--- 프로그래머스/120889: 삼각형의 완성조건 (1).java
+++ 프로그래머스/120889: 삼각형의 완성조건 (1).java
@@ -1,13 +1,10 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] sides) {
-        int big_idx = 0;
-        for (int i=0; i<2; i++) {
-            if (sides[i] < sides[i+1]) big_idx = i+1;
-            else big_idx = i;
-        }
+        Arrays.sort(sides);
         int answer = 1;
-        int side = sides[0] + sides[1] + sides[2] - sides[big_idx];
-        if (side <= sides[big_idx]) answer = 2;
+        if (sides[0] + sides[1] <= sides[2]) answer = 2;
         return answer;
     }
 }
