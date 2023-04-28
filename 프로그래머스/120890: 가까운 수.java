8b27dc779c96914bf5c1229ca93309c4a4541971
Jungmin-Hong <jungmin011107@gmail.com>
2023-04-28 21:27:47 +0900

    solved: [PGS] 120890

--- 프로그래머스/120890: 가까운 수.java
+++ 프로그래머스/120890: 가까운 수.java
@@ -1,6 +1,6 @@
 class Solution {
     public int solution(int[] array, int n) {
-        int index = 0;
+        int index = 0; 
         int gap = n;
         for (int i=0; i<array.length; i++) {
             if (array[i] >= n){
@@ -8,12 +8,14 @@ class Solution {
                     index = i;
                     gap = array[i]-n;
                 }
+                if (array[i]-n == gap && array[index] > array[i]) index = i;
             }
             else {
                 if (n-array[i] < gap) {
                     index = i; 
                     gap = n-array[i];
                 }
+                if (n-array[i] == gap && array[index] > array[i]) index = i;
             }   
         }
         int answer = array[index];
