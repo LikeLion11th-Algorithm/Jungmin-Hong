a143c97879fa10c38baa9215eb785d61ea51601e
Jungmin-Hong <jungmin011107@gmail.com>
2023-05-02 16:02:21 +0900

    another: [PGS] 120906

--- 프로그래머스/120906: 자릿수 더하기.java
+++ 프로그래머스/120906: 자릿수 더하기.java
@@ -1,10 +1,9 @@
 class Solution {
     public int solution(int n) {
-        String[] str = Integer.toString(n).split("");
         int answer = 0;
-        
-        for (int i=0; i<str.length; i++) {
-            answer += Integer.parseInt(str[i]);
+        while (n>0) {
+            answer += n%10;
+            n /= 10;
         }
         
         return answer;
