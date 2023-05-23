f1df651ce8558345aaabdb089fd64a42b4f6305c
Jungmin-Hong <jungmin011107@gmail.com>
2023-05-23 12:07:13 +0900

    solved: [PGS] 181928

---
+++ 프로그래머스/181928: 이어 붙인 수.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] num_list) {
+        String even = "";
+        String odd = "";
+        
+        for (int n : num_list) {
+            if (n%2==0) even += n;
+            else odd += n;
+        }
+        
+        return Integer.parseInt(even)+Integer.parseInt(odd);
+    }
+}
