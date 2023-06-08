b765bec0ba12743cfddd0332f5cd89fa8e2524b2
Jungmin-Hong <jungmin011107@gmail.com>
2023-06-08 12:48:52 +0900

    solved: [PGS] 181883

---
+++ 프로그래머스/181883: 수열과 구간 쿼리 1.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] queries) {
+        for (int i=0; i<queries.length; i++) {
+            for (int k=queries[i][0]; k<=queries[i][1]; k++) {
+                arr[k]++;
+            }
+        }
+        return arr;
+    }
+}
