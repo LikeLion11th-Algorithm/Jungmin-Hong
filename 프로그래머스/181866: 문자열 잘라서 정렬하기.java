6a7300c78a6853ad23f6a068eff4fb64fc02bc83
Jungmin-Hong <jungmin011107@gmail.com>
2023-06-12 13:02:26 +0900

    solved: [PGS] 181866

--- 프로그래머스/181866: 문자열 잘라서 정렬하기.java
+++ 프로그래머스/181866: 문자열 잘라서 정렬하기.java
@@ -2,6 +2,7 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String myString) {
+        myString = myString.replaceFirst("^x+", "");
         String[] answer = myString.split("[x]+");
         Arrays.sort(answer);
         return answer;
