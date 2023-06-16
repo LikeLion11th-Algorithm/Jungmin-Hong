58a36fd8b64db65f163f694ad66eedffe475b8ca
Jungmin-Hong <jungmin011107@gmail.com>
2023-06-16 11:45:48 +0900

    solved: [PGS] 181849

---
+++ 프로그래머스/181849: 문자열 정수의 합.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(String num_str) {
+        int answer = 0;
+        String[] num = num_str.split("");
+        for (String n : num) {
+            answer += Integer.parseInt(n);
+        }
+        return answer;
+    }
+}
