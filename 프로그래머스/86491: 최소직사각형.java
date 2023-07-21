4226a2a451b61a1818ded7396c02d24c219a79d9
Jungmin-Hong <jungmin011107@gmail.com>
2023-07-21 10:15:10 +0900

    solved: [PGS] 86491

---
+++ 프로그래머스/86491: 최소직사각형.java
@@ -0,0 +1,13 @@
+import java.math.*;
+
+class Solution {
+    public int solution(int[][] sizes) {
+        int max = 0;
+        int min = 0;
+        for (int i=0; i<sizes.length; i++) {
+            max = Math.max(max,Math.max(sizes[i][0],sizes[i][1]));
+            min = Math.max(min,Math.min(sizes[i][0],sizes[i][1]));
+        }
+        return max*min;
+    }
+}
