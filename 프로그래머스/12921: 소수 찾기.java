03d89842cfcd548a73140cf448b42d1494612f30
Jungmin-Hong <jungmin011107@gmail.com>
2023-07-29 10:13:14 +0900

    solved: [PGS] 12921

---
+++ 프로그래머스/12921: 소수 찾기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        for (int i=2; i<=n; i++) {
+            if (isPrimeNum(i)) answer++;
+        }
+        return answer;
+    }
+    
+    public boolean isPrimeNum(int n) {
+        for (int i=2; i<=Math.sqrt(n); i++) {
+            if (n%i == 0) return false;
+        }
+        return true;
+    }
+}
