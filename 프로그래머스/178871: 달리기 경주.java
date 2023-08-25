2ef6db962079c2dfcf2c2dc3e397f7eda9395ee9
Jungmin-Hong <jungmin011107@gmail.com>
2023-08-25 12:30:24 +0900

    solved: [PGS] 178871

--- 프로그래머스/178871: 달리기 경주.java
+++ 프로그래머스/178871: 달리기 경주.java
@@ -3,8 +3,17 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] players, String[] callings) {
         
+        Map<String, Integer> playerRank = new HashMap<>();
+
+        // 선수들의 등수를 초기화
+        for (int i = 0; i < players.length; i++) {
+            playerRank.put(players[i], i);
+        }
+        
         for (int i=0; i<callings.length; i++) {
-            int idx = Arrays.asList(players).indexOf(callings[i]);
+            int idx = playerRank.get(callings[i]);
+            playerRank.put(callings[i], idx-1);
+            playerRank.put(players[idx-1], idx);
             swap(idx, idx-1, players);
         }
         
