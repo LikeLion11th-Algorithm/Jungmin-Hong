9374560db25f2fbae20095c590ab6193b0a54c4c
Jungmin-Hong <jungmin011107@gmail.com>
2023-09-02 11:45:15 +0900

    solved: [PGS] 12930

--- 프로그래머스/12930: 이상한 문자 만들기.java
+++ 프로그래머스/12930: 이상한 문자 만들기.java
@@ -1,14 +1,20 @@
 class Solution {
     public String solution(String s) {
-        String[] answer = s.split("\\s+");
+        String[] answer = s.split("");
+        int idx = 0;
+        
+        // 1. 띄어쓰기가 아니면 인덱싱 시작
+        // 2. 인덱스가 짝수면 대문자
+        // 3. 인덱스가 홀수면 소문자로 변경
+        
         for (int i=0; i<answer.length; i++) {
-            String word = "";
-            String[] chars = answer[i].split("");
-            for (int k=0; k<chars.length; k++) {
-                word += k%2==0 ? chars[k].toUpperCase() : chars[k].toLowerCase();
+            if (answer[i].equals(" ")) idx=0;
+            else {
+                if (idx%2==0) answer[i] = answer[i].toUpperCase();
+                else answer[i] = answer[i].toLowerCase();
+                idx++;
             }
-            answer[i] = word;
         }
-        return String.join(" ", answer);
+        return String.join("", answer);
     }
 }
