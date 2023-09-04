049d67876679b5f3ef4b84735335b49bb22a930d
Jungmin-Hong <jungmin011107@gmail.com>
2023-09-04 20:55:16 +0900

    failed: [PGS] 12945

--- 프로그래머스/12945: 피보나치 수.java
+++ 프로그래머스/12945: 피보나치 수.java
@@ -1,9 +1,18 @@
+import java.util.*;
+
 class Solution {
+    int[] fibo1;
     public int solution(int n) {
-        return fibo(n);
+        fibo1 = new int[n + 1];
+        Arrays.fill(fibo1, -1);
+        fibo1[0] = 0;
+        fibo1[1] = 1;
+        return fibo2(n)%1234567;
     }
     
-    public int fibo(int n) {
-        return n>=2 ? fibo(n-1)+fibo(n-2) : n;
+    public int fibo2(int n) {
+        if (fibo1[n]==-1) fibo1[n] = fibo2(n-1)+fibo2(n-2);
+        return fibo1[n];
     }
+    
 }
