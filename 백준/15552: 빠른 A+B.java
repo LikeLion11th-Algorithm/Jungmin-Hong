955f29397b973f0cfc52f88bb8ac0d73937f5638
Jungmin-Hong <jungmin011107@gmail.com>
2023-09-21 00:41:09 +0900

    solved: [BOJ] 15552

---
+++ 백준/15552: 빠른 A+B.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); // 선언
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));   //할당된 버퍼에 값 넣어주기
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            bw.write(a+b+"\n");
+        }
+        bw.close();
+    }
+}
