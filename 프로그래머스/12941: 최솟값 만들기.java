b90c082914d56ddb52dc0859b9e13fd763615bf3
Jungmin-Hong <jungmin011107@gmail.com>
2023-11-09 19:44:36 +0900

    solved: [PGS] 12941

---
+++ 프로그래머스/12941: 최솟값 만들기.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for (int i=1; i<=A.length; i++) {
+            answer += A[i-1]*B[B.length-i];
+        }
+
+        return answer;
+    }
+}
