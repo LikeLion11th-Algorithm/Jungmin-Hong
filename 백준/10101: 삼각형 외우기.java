9e2014aa2525ac0a5bcd3d4422b24e4915cc4674
Jungmin-Hong <jungmin011107@gmail.com>
2023-11-29 14:17:45 +0900

    solved: [BOJ] 10101

---
+++ 백준/10101: 삼각형 외우기.java
@@ -0,0 +1,20 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        if (a+b+c != 180) bw.write("Error");
+        else if (a==b && b==c) bw.write("Equilateral");
+        else if (a==b || b==c || a==c) bw.write("Isosceles");
+        else bw.write("Scalene");
+
+        bw.flush();
+        bw.close();
+    }
+}
