33fa0f7abc9278ce1525a797879d3268e3b3fa02
Jungmin-Hong <jungmin011107@gmail.com>
2024-04-09 13:16:01 -0700

    solved: [PGS] 17681

---
+++ 프로그래머스/17681: [1차] 비밀지도.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
+                
+        for (int i=0; i<n; i++) {
+            answer[i] = String.format("%"+n+"s", Integer.toBinaryString(arr1[i] | arr2[i]));
+            answer[i] = answer[i].replaceAll("0", " ");
+            answer[i] = answer[i].replaceAll("1", "#");
+        }
+        
+        return answer;
+    }
+}
