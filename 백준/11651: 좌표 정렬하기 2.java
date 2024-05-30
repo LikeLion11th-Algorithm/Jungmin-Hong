54952ab26eb4db188d4fd44aff833c1a10b159f3
Jungmin-Hong <jungmin011107@gmail.com>
2024-05-29 17:33:25 -0700

    solved: [BOJ] 11651

---
+++ 백준/11651: 좌표 정렬하기 2.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] n = new int[N][2];
+
+        for (int i=0; i<N; i++) {
+            String[] str = br.readLine().split(" ");
+            n[i][0] = Integer.parseInt(str[0]);
+            n[i][1] = Integer.parseInt(str[1]);
+        }
+
+        Arrays.sort(n, Comparator.comparingInt(num -> num[0]));
+        Arrays.sort(n, Comparator.comparingInt(num -> num[1]));
+
+        for (int i=0; i<N; i++) {
+            bw.write(n[i][0] + " " + n[i][1] + "\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
