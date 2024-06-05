3796001db0d5a21fdd1f220b5c758e9fceb910af
Jungmin-Hong <jungmin011107@gmail.com>
2024-06-05 11:21:45 -0700

    failed: [BOJ] 18870

--- 백준/18870: 좌표 압축.java
+++ 백준/18870: 좌표 압축.java
@@ -9,19 +9,17 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         String[] s = br.readLine().split(" ");
-        ArrayList<Integer> n = new ArrayList<>();
         ArrayList<Integer> x = new ArrayList<>();
-
+        TreeSet<Integer> treeSet = new TreeSet<Integer>();
 
         for (int i=0; i<N; i++) {
             int num = Integer.parseInt(s[i]);
-            if (!n.contains(num)) n.add(num);
+            treeSet.add(num);
             x.add(num);
         }
-        Collections.sort(n);
 
         for (int i=0; i<x.size(); i++) {
-            bw.write(n.indexOf(x.get(i)) + " ");
+            bw.write(treeSet.headSet(x.get(i)).size() + " ");
         }
 
         bw.flush();
