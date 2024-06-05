4d541a118e543c7da924a475d8b8f5314d70b4a9
Jungmin-Hong <jungmin011107@gmail.com>
2024-06-05 11:36:31 -0700

    solved: [BOJ] 18870

--- 백준/18870: 좌표 압축.java
+++ 백준/18870: 좌표 압축.java
@@ -9,19 +9,32 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
         String[] s = br.readLine().split(" ");
-        ArrayList<Integer> x = new ArrayList<>();
-        TreeSet<Integer> treeSet = new TreeSet<Integer>();
+        int[] x = new int[N];
 
         for (int i=0; i<N; i++) {
-            int num = Integer.parseInt(s[i]);
+            x[i] = Integer.parseInt(s[i]);
+        }
+
+        // 중복 제거 및 정렬하여 숫자의 index 파악
+        TreeSet<Integer> treeSet = new TreeSet<>();
+        for (int num : x) {
             treeSet.add(num);
-            x.add(num);
         }
 
-        for (int i=0; i<x.size(); i++) {
-            bw.write(treeSet.headSet(x.get(i)).size() + " ");
+        // 각 숫자와 해당 숫자의 index 저장
+        HashMap<Integer, Integer> indexMap = new HashMap<>();
+        int index = 0;
+        for (int num : treeSet) {
+            indexMap.put(num, index++);
         }
 
+        // 배열 순서대로 숫자의 index를 불러 문자열 생성
+        StringBuilder sb = new StringBuilder();
+        for (int num : x) {
+            sb.append(indexMap.get(num)).append(" ");
+        }
+
+        bw.write(sb.toString().trim());
         bw.flush();
         bw.close();
     }
