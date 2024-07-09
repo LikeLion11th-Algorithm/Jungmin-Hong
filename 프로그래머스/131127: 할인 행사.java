9eb528035ffba1fe8ba357d33b272236f98982a3
Jungmin-Hong <jungmin011107@gmail.com>
2024-07-09 09:52:55 -0700

    solved: [PGS] 131127

--- 프로그래머스/131127: 할인 행사.java
+++ 프로그래머스/131127: 할인 행사.java
@@ -3,19 +3,19 @@ import java.util.*;
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        Queue<String> q = new LinkedList<>();
-
-        for (int i=0; i<discount.length-9; i++) {
-            for (int n=0; n<want.length; n++) {
-                for (int k=0; k<number[n]; k++) q.add(want[n]);
-            }
-            for (int k=0; k<10; k++) {
-                if (q.contains(discount[k+i])) q.remove(discount[k+i]);
-            }
-            if (q.isEmpty()) answer++;
-            q.clear();
+        ArrayList<String> list = new ArrayList<>();
+        for (int i=0; i<want.length; i++) {
+            for (int k=0; k<number[i]; k++) list.add(want[i]);
         }
-
+        
+        for (int i=0; i<discount.length-9; i++) {
+            ArrayList<String> copy = (ArrayList<String>) list.clone();
+            for (int k=0; k<10; k++) {
+                if (copy.contains(discount[k+i])) copy.remove(discount[k+i]);
+            }
+            if (copy.size()==0) answer++;
+        }
+        
         return answer;
     }
 }
