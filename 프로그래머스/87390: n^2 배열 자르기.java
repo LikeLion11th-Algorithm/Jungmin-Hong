b8bf1ff2e2b6da98ed1c8169d3d5f5695c013cb2
Jungmin-Hong <jungmin011107@gmail.com>
2024-09-30 13:02:47 +0900

    solved: [BOJ] 87390

--- 프로그래머스/87390: n^2 배열 자르기.java
+++ 프로그래머스/87390: n^2 배열 자르기.java
@@ -4,19 +4,14 @@ class Solution {
     public int[] solution(int n, long left, long right) {
 
         int[] answer = new int[(int)(right-left)+1];
-        int[][] arr = new int[n][n];
         
-        for (int i=0; i<n; i++) {
-            for (int j=0; j<n; j++) {
-                arr[i][j] = Math.max(i,j)+1;
-            }
-        }
-        
-        int[] array = Arrays.stream(arr)
-        .flatMapToInt(Arrays::stream)
-        .toArray();
-        
-        answer = Arrays.copyOfRange(array, (int)left, (int)right+1);
+        // 메모리 초과를 피하기 위해 필요한 부분의 배열 값만 얻어오기
+        int idx = 0;
+        for (long i=left; i<=right; i++) {
+            int row = (int) (i/n);
+            int col = (int) (i%n);
+            answer[idx++] = Math.max(row,col)+1;
+        }       
         
         return answer;
     }
