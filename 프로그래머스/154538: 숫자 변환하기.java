6a4cf02e15d708a9d9fbfbc7788881ab0363e7ad
Jungmin-Hong <jungmin011107@gmail.com>
2024-10-05 22:48:08 +0900

    solved: [PGS] 154538

--- 프로그래머스/154538: 숫자 변환하기.java
+++ 프로그래머스/154538: 숫자 변환하기.java
@@ -1,22 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int solution(int x, int y, int n) {
-        int answer = -1;
+        int[] dp = new int[y+1];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[x] = 0;
         
-        if (y%x==0 && (y/x)%2==0) {
-            answer = (y/x)/2;
-            System.out.println("2");
+        for (int i=x; i<=y; i++) {
+            // 애초에 확인할 값이 구해지지 않은 값이라면 굳이 할 필요 X
+            if (dp[i]==Integer.MAX_VALUE) continue;
+            
+            if (i+n<=y) dp[i+n] = Math.min(dp[i]+1, dp[i+n]);
+            if (i*2<=y) dp[i*2] = Math.min(dp[i]+1, dp[i*2]);
+            if (i*3<=y) dp[i*3] = Math.min(dp[i]+1, dp[i*3]);
         }
-        if (y%x==0 && (y/x)%3==0) {
-            if (answer==-1) answer = (y/x)/3;
-            else answer = answer<(y/x)/3 ? answer:(y/x)/3;
-            System.out.println("3");
-        }
-        if ((y-x)%n==0) {
-            if (answer==-1) answer = (y-x)/n;
-            else answer = answer<(y-x)/n ? answer:(y-x)/n;
-            System.out.println("n");
-        }
+        
+        if (dp[y]==Integer.MAX_VALUE) return -1;
                 
-        return answer;
+        return dp[y];
     }
 }
